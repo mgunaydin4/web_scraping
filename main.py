57e6684687df168ff204703dd438aaeb08096e1d
Melih Günaydın <84282430+mgunaydin4@users.noreply.github.com>
2024-12-10 20:27:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-################################
-# İş Problemi
-################################
-
-# Online kitap satışı yapan bir şirket, “Seyahat” ve “Kurgu Dışı” kategorilerinde az satış yaptığını tespit
-# ediyor. Bu sebeple şirketin, rakip firmanın kazınması izin verilen https://books.toscrape.com/ adlı web
-# sitesinden “Travel” ve “Nonfiction” kategorisindeki kitaplara ait bilgileri alıp rakip ve fiyat analizi
-# yapmaya ihtiyacı var. Şirket sizden, bu kategorilerdeki her bir kitaba ait detay sayfalarına gitmenizi ve
-# orada bulunan bazı bilgileri almanızı bekliyor.
-
-#######################
-# Proje Akışı
-#######################
-
-# 1. "Travel" ve "Nonfiction" kategorilerine ait kitapların yer aldığı sayfa linkleri ana sayfa içerisinden alınmalı.
-# 2. Sonrasında ilk kategoriye ait ürünlerin bulunduğu sayfa görüntülenerek, tüm kitapların detay sayfalarına erişmek için linkler kazınmalı.
-# 3. Kazınan linkler kullanılarak o kategoriye ait tüm kitapların detay bilgileri kazınmalı.
-# 4. Adım 2 ve 3’ü diğer kategori için de yapabilmek adına sistem otomatize edilmeli.
-
 # Kütüphanelerin import edilmesi
 from selenium import webdriver
 import time
